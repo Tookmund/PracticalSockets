abfacaea374f4556fb71ee691095902ca4b745ed
Jacob Adams <Tookmund@users.noreply.github.com>
2014-11-05 09:55:29 -0500

    Update UDPEchoClient.cpp

--- examples/UDPEchoClient.cpp
+++ examples/UDPEchoClient.cpp
@@ -20,7 +20,7 @@
 #include <PracticalSockets.h>      // For UDPSocket and SocketException
 #include <iostream>               // For cout and cerr
 #include <cstdlib>                // For atoi()
-#include <cstring>
+#include <cstring>                // For strlen()
 
 using namespace std;
 
